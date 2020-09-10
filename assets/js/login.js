69ce56b529e4c7ea7616ed052434cc086b4bdb8c
Dd <1522352494@qq.com>
2020-09-10 09:08:05 +0800

    搭建注册/登录静态页面

---
+++ assets/js/login.js
@@ -0,0 +1,8 @@
+$('#link_login').click(function() {
+    $('.reg-box').hide()
+    $('.login-box').show()
+})
+$('#link_reg').click(function() {
+    $('.reg-box').show()
+    $('.login-box').hide()
+})
